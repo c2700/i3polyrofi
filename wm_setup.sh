bce083733070566fd67b22f97979206e5e35ef92
c2700 <rohanphilip.india@gmail.com>
2021-08-16 23:56:06 +0530

    started setup.sh

--- wm_setup.sh
+++ wm_setup.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
-# look for wlan interfaces
+# search for collect interfaces
+ifaces=($(nmcli device show | grep -i "general.device\|general.type" | grep -iv "p2p\|p2p-\|lo$\|loopback" | awk '{ print $2 }'))
 
-# look for eth interfaces
+wifi_ifaces=()
+ethernet_ifaces=()
+
+a=0
+for ((i=1; i <= ${#ifaces[@]}; i+2 ))
+do
+	if [[ ${ifaces[$i]} == "wifi" ]]
+	then
+		a=$i
+		a=$((a-1))
+		wifi_ifaces+=("${ifaces[$a]}")
+	elif [[ ${ifaces[$i]} == "ethernet" ]]
+	then
+		a=$i
+		a=$((a-1))
+		ethernet_ifaces+=("${ifaces[$a]}")
+	fi
+done
+unset ifaces
+
+# loop to select wifi_iface
+# set wireless iface
+sed '276s/interface = /interface = /g'
+
+# loop to select ether_iface
+# set wireless iface
+sed '282s/interface = /interface = /g'
 
 # users to setup this dotfile for
 
