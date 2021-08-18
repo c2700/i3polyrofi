3a05d58856f8bebb8f61a2acd1adf39835b2db33
c2700 <rohanphilip.india@gmail.com>
2021-08-19 02:31:54 +0530

    made some more changes

--- README.md
+++ README.md
@@ -6,3 +6,43 @@ If you ever feel like you can't find the fonts you are looking just head over to
 <h2>idotfiles setup</h2><br>
 chmod a+x wm_setup.sh<br>
 ./wm_setup.sh<br>
+
+wifi module:
+	left click - opens dunst notification get pvt ipv4, ipv6 addresses, ssid connected to and checks for 
+				 WAN connectivity (internet).
+	right click - disconnects wifi
+
+ethernet module:
+	left click - same as wifi module's left click
+
+calendar:
+	left click - opens calendar in dunst notification
+
+disk module:
+	left click - shows all disks and partitions and their mountpoints in brackets (if mounted) next to the
+				 partition
+
+init script - one time run on startup and exit on execution. shows, batteyr, wifi and ethernet stats
+
+arch logo - left click to spawn rofi
+
+shortcuts:
+	super+r - resize windows
+	super+f (or) <F11> - full screen window
+	super+shift+space - toggle floating window mode
+	super+shift+arrow_key - move floating window in set direction
+	super+shift+e - exit i3 without prompt (comment line 155, uncomment line 154 if you need the exit
+											prompt)
+	ctrl+shift+delete - shutdown machine (i3 keybinding)
+	ctrl+alt+delete - reboot machine (i3 keybinding)
+	super+shift+o - turns on all screens (script is i3/scripts/screens/turn_on_all_screens.sh)
+	super+shift+b - turns off all screens (script is i3/scripts/screens/turn_off_all_screens.sh)
+	super+shift+a - turns off only primary screen (script is i3/scripts/screens/turn_off_main_screen.sh)
+	super+shift+q - quits focused app
+	super+arrow_keys - change window focus
+	ctrl+alt+f - open rofi in file explorer mode
+	super+enter - open terminal emulator
+	super+shift+r - restart i3
+	ctrl+shift+esc - starts htop in set terminal emulator
+
+	
--- wm_setup.sh
+++ wm_setup.sh
@@ -1,63 +1,125 @@
 #!/bin/bash
 
-# set wireless iface
-wifi_iface=("$(nmcli device | grep -i "wifi" | grep -iv "p2p" | awk '{ print $1 }')")
-if [[ ${#wifi_iface[@]} -eq 1 ]]
+if [[ $DESKTOP_SESSION != "i3" ]]
 then
-	cat polybar/main_config | sed '270s/interface = /interface = $wifi_iface/g'
-	# sed '270s/interface = /interface = $wifi_iface/g'
-	echo "Wi-Fi card set to $wifi_iface"
-	unset wifi_iface
-# loop to select wifi_iface
-elif [[ ${#wifi_iface[@]} -gt 1 ]]
+	echo "This rice setup is for i3-gaps Window Manager. Please run Your Linux machine on i3-gaps Window Manager"
+	echo "Checking if i3 -gaps window manager is installed"
+	sudo pacman --noconfirm -Qs i3-gaps
+	case $? in
+		0) echo "i3-gaps is installed. please kill the current session and login with i3-gaps running (i3-gaps could be shown as "i3" on your display manager's "session menu")" ;;
+		1) 
+			read "i3 is not installed. Install i3? [y/n]" choice
+			case $choice in
+				'y'|'Y'|['yY']['eE']['sS']) 
+					sudo pacman -Syvd --noconfirm i3-gaps
+					echo "i3 has been installed"
+					;;
+				'n'|'N'|['nN']['oO']) echo "i3-gaps needs to be installed and run for this setup to run" ;;
+			esac
+			;;
+	esac
+	exit
+
+elif [[ $DESKTOP_SESSION == "i3" ]]
 then
-	a=1
-	for i in ${wifi_iface[@]}
-	do
-		echo " $a) $i"
-		a=$((a+1))
-	done
-	echo ""
-	read -p "select wireless interface: " selected_iface
-	selected_iface=$((selected_iface-1))
- 	cat polybar/main_config | sed '270s/interface = /interface = ${wifi_iface[$selected_iface]}/g'
-	echo "Wi-Fi card set to $wifi_iface"
-	unset a selected_iface wifi_iface
+	echo "installing dunst rofi, rofi-calc and rofi-file-browser-extended-git"
+	sudo pacman -Syvd --noconfirm dunst rofi rofi-calc
+	git clone https://github.com/marvinkreis/rofi-file-browser-extended
+	cd rofi-file-browser-extended
+	makepkg -i
+	# makepkg -sdi
+	# makepkg -sd
+	# sudo pacman -Uvd --noconfirm *rofi-file-browser*.pkg.tar.*
+	cd ..
+	
+	terms=($(pqqs "terminal emulator" | grep -iv "lib"))
+	if [[ ${#terms[@]} -eq 0 ]]
+	then
+		echo "no terminal emulators found. Installing vte3"
+		sudo pacman -Syvd --noconfirm vte3
+		VTE3="$(pacman -Ql vte3 | grep -iv "bin/$" | grep -i "bin" | awk '{ print $2 }')"
+		cat i3/config | sed 's/konsole/$VTE3/g'
+		# sed -i i3/config 's/konsole/$VTE3/g'
+	elif [[ ${#terms[@]} -eq 1 ]]
+	then
+		sed -i i3/config 's/konsole/${terms[0]}/g'
+		# cat i3/config | sed 's/konsole/${terms[0]}/g'
+		echo "terminal set to ${terms[0]}"
+	elif [[ ${#terms[@]} -gt 1 ]]
+	then
+		a=1
+		for i in ${terms[@]}
+		do
+			echo " $a) $i"
+			a=$((a+1))
+		done
+		echo ""
+		read -p "select terminal emulator to use: " selected_terminal
+		selected_terminal=$((selected_terminal-1))
+	 	cat i3/config | sed 's/konsole/${terms[$(selected_terminal)]}/g'
+	 	# sed -i i3/config 's/konsole/${terms[$(selected_terminal)]}/g'
+		echo "terminal set to ${terms[$(selected_terminal)]}"
+		unset a selected_terminal terms
+	fi
+
+	# set wireless iface
+	wifi_iface=("$(nmcli device | grep -i "wifi" | grep -iv "p2p" | awk '{ print $1 }')")
+	if [[ ${#wifi_iface[@]} -eq 1 ]]
+	then
+		cat polybar/main_config | sed '270s/interface = /interface = ${wifi_iface[0]}/g'
+		echo "Wi-Fi card set to ${wifi_iface[0]}"
+		unset wifi_iface
+	# loop to select wifi_iface
+	elif [[ ${#wifi_iface[@]} -gt 1 ]]
+	then
+		a=1
+		for i in ${wifi_iface[@]}
+		do
+			echo " $a) $i"
+			a=$((a+1))
+		done
+		echo ""
+		read -p "select wireless interface: " selected_iface
+		selected_iface=$((selected_iface-1))
+	 	cat polybar/main_config | sed '270s/interface = /interface = ${wifi_iface[$selected_iface]}/g'
+		echo "Wi-Fi card set to ${wifi_iface[$selected_iface]}"
+		unset a selected_iface wifi_iface
+	fi
+	
+	# loop to select 
+	ether_iface=("$(nmcli device | grep -i "ethernet" | grep -iv "p2p" | awk '{ print $1 }')")
+	if [[ ${#ether_iface[@]} -eq 1 ]]
+	then
+		cat polybar/main_config | sed '282s/interface = /interface = ${ether_iface[0]}/g'
+		echo "Ethernet card set to ${ether_iface[0]}"
+		unset ether_iface
+	# set ether_iface iface
+	elif [[ ${#ether_iface[@]} -gt 1 ]]
+	then
+		a=1
+		for i in ${ether_iface[@]}
+		do
+			echo " $a) $i"
+			a=$((a+1))
+		done
+		echo ""
+		read -p "select ethernet interface: " selected_iface
+		selected_iface=$((selected_iface-1))
+		cat polybar/main_config | sed '282s/interface = /interface = ${ether_iface[$selected_iface]}/g'
+		echo "Ethernet card set to ${ether_iface[$selected_iface]}"
+		unset a selected_iface ether_iface
+	fi
+	
+	cat i3/config | awk '{ sub("home/blank","home/$(whoami)"); print $0 }'
+	cat polybar/launch.sh | awk '{ sub("home/blank","home/$(whoami)"); print $0 }'
+	cat polybar/main_config | awk '{ sub("home/blank","home/$(whoami)"); print $0 }'
+	cat dunst/dunstrc | awk '{ sub("home/blank","home/$(whoami)"); print $0 }'
+	
+	# sed -i i3/config 's/"home\/blank","home\/$(whoami)")/g'
+	# sed -i polybar/launch.sh 's/"home\/blank","home\/$(whoami)"/g'
+	# sed -i polybar/main_config 's/"home\/blank","home\/$(whoami)"/g'
+	# sed -i dunst/dunstrc 's/"home\/blank","home\/$(whoami)"/g'
+	
+	echo "cp -rfv polybar i3 rofi -t \"/home/$(whoami)/.config\""
 fi
-
-# loop to select 
-ether_iface=("$(nmcli device | grep -i "ethernet" | grep -iv "p2p" | awk '{ print $1 }')")
-if [[ ${#ether_iface[@]} -eq 1 ]]
-then
-	cat polybar/main_config | sed '282s/interface = /interface = $ether_iface/g'
-	echo "Ethernet card set to ${ether_iface[0]}"
-	unset ether_iface
-# set ether_iface iface
-elif [[ ${#ether_iface[@]} -gt 1 ]]
-then
-	a=1
-	for i in ${ether_iface[@]}
-	do
-		echo " $a) $i"
-		a=$((a+1))
-	done
-	echo ""
-	read -p "select ethernet interface: " selected_iface
-	selected_iface=$((selected_iface-1))
-	cat polybar/main_config | sed '282s/interface = /interface = ${ether_iface[$selected_iface]}/g'
-	echo "Ethernet card set to $ether_iface"
-	unset a selected_iface ether_iface
-fi
-
-# cat i3/config | awk '{ sub("home/blank","home/$(whoami)"); print $0 }'
-# cat polybar/launch.sh | awk '{ sub("home/blank","home/$(whoami)"); print $0 }'
-# cat polybar/main_config | awk '{ sub("home/blank","home/$(whoami)"); print $0 }'
-# cat dunst/dunstrc | awk '{ sub("home/blank","home/$(whoami)"); print $0 }'
-
-sed -i i3/config 's/"home\/blank","home\/$(whoami)")/g'
-sed -i polybar/launch.sh 's/"home\/blank","home\/$(whoami)"/g'
-sed -i polybar/main_config 's/"home\/blank","home\/$(whoami)"/g'
-sed -i dunst/dunstrc 's/"home\/blank","home\/$(whoami)"/g'
-
-echo "cp -rfv polybar i3 rofi -t \"/home/$(whoami)/.config\""
 
