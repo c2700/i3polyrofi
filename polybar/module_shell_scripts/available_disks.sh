08611d56a5fc4be547a8dcb5d759f9a892415273
c2700 <rohanphilip.india@gmail.com>
2021-08-22 00:49:51 +0530

    updated

--- polybar/module_shell_scripts/available_disks.sh
+++ polybar/module_shell_scripts/available_disks.sh
@@ -2,6 +2,8 @@
 
 disks=($(lsblk -dnlo name | grep -iv "loop\|sr0"))
 diskinfo_array=()
+head_lines=""
+max_length=""
 for i in ${disks[@]}
 do
 	dev_iface=""
@@ -26,11 +28,29 @@ do
 			"sata") dev_iface="SATA drive" ;;
 		esac
 	fi
-	diskinfo_array+=("$(echo $disk_name [/dev/$i] - $disk_size - $dev_pttype - $dev_iface | sed 's/\s\s*/ /g')")
+	disk_info="  $(echo $disk_name [/dev/$i] - $disk_size - $dev_pttype - $dev_iface | sed 's/\s\s*/ /g')\n"
+	disk_info_temp="$disk_info"
+ 	export disk_info_temp
+ 	length="$(python -c 'from os import environ;print((len(environ["disk_info_temp"]))-3)')"
+ 	unset disk_info_temp
+	max_length=$length
+
+	[[ $max_length < $length ]] && max_length=$length
+
+	disk_info+="   "
+	if [[ "$i" != "${disks[-1]}" ]]
+	then
+	 	for (( j=0; j <= $length; j++ )){
+	 		disk_info+="-"
+	 	}
+	fi
+	diskinfo_array+=("$disk_info\n")
+	unset disk_info
 done
 
-for i in "${diskinfo_array[@]}"
-do
-	notify-send -u low " $i "
-done
+for (( j=0;j<=$max_length;j++ )){
+	head_lines+="-"
+}
+
+notify-send -u low " Attached Disks " " ${head_lines}\n ${diskinfo_array[*]} "
 
